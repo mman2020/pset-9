da4271c03dec47170bd4cecc24ead0635d636d48
Matthew Man <mman@ucvts.org>
2020-02-03 08:36:57 -0500

    completed exercise 10

--- src/Exercises.java
+++ src/Exercises.java
@@ -229,7 +229,33 @@ public class Exercises {
 
 	public int clumps(ArrayList<String> values) {
 		// write your code here
+    if (values == null) {
+			return -1;
+		}
+		for (int i = 0; i < values.size(); i++) {
+			if (values.get(i) == null) {
+				return -1;
+			}
+		}
 
-		return -1;		// default return value to ensure compilation
+		boolean isSame = false;
+		boolean prevSame = false;
+		String previous = "";
+		int clumps = 0;
+
+		for (int i = 0; i < values.size(); i++) {
+			if (previous.equals(values.get(i))) {
+				isSame = true;
+				if (prevSame != true) {
+					clumps++;
+				}
+			} else {
+				isSame = false;
+			}
+			previous = values.get(i);
+			prevSame = isSame;
+		}
+
+		return clumps;
 	}
 }
